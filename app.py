8b859359b32a47b2429f2e9b0033793a8ee37839
Isaiah Gonzales <110946128+Isaiah-Gonzales@users.noreply.github.com>
2024-05-03 15:37:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Artificial Neural Network: Flowability Predictor")
 
-drug = st.selectbox('Please select your drug:', ('drug1', 'drug2')
+drug = st.selectbox('Please select your drug:', ('drug1', 'drug2'))
         
