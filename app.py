81bae9f3c7fe917d896255fef4ceb422ae0fcb80
Isaiah Gonzales <110946128+Isaiah-Gonzales@users.noreply.github.com>
2024-05-07 09:28:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ import streamlit as st
 
 st.title("Artificial Neural Network: Flowability Predictor")
 
-drug = st.selectbox('Please select your drug:', ('drug1', 'drug2'))
+drug = st.selectbox('Please select your drug:', ('ibupfrofen', 'metformin', 'input my own drug characteristics'))
+
+target_solid_fraction = st.text_input('target solid fraction (default 0.7):', value = 0.7)
         
