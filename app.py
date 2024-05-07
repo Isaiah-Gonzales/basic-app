8586e9a6e456bd6815bd50d9d7a81c71d11a9e02
Isaiah Gonzales <110946128+Isaiah-Gonzales@users.noreply.github.com>
2024-05-07 09:34:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ st.title("Artificial Neural Network: Flowability Predictor")
 
 drug = st.selectbox('Please select your drug:', ('ibupfrofen', 'metformin', 'input my own drug characteristics'))
 
+if drug == 'input my own drug characteristics':
+        drug_d90 = st.text_input('drug d90')
+        ib_bulk_d = st.text_input('initial blend bulk density')
+        ib_flow_rate = st.text_input('initial blend flow rate')
+
 target_solid_fraction = st.text_input('target solid fraction (default 0.7):', value = 0.7)
         
