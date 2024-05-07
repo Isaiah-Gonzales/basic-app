2cf18df2907b461753964150906f475b6e9b86b3
Isaiah Gonzales <110946128+Isaiah-Gonzales@users.noreply.github.com>
2024-05-07 09:39:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("Artificial Neural Network: Flowability Predictor")
+st.title("Artificial Neural Network: Roller Compactor Tool")
+
+predictor = st.selectbox('what would you like to predict:', ('flowability (I have a target solid fraction and roll pressure in mind)','roll pressure (I have a target solid fraction and flowability in mind)')
 
 drug = st.selectbox('Please select your drug:', ('ibupfrofen', 'metformin', 'input my own drug characteristics'))
 
@@ -10,4 +12,4 @@ if drug == 'input my own drug characteristics':
         ib_flow_rate = st.text_input('initial blend flow rate')
 
 target_solid_fraction = st.text_input('target solid fraction (default 0.7):', value = 0.7)
-        
+
